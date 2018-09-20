682cb2092a5fd6079224d8a98225c30092769408
yongjie_zou <zouyongjie@haixue.com>
2018-09-20 16:13:56 +0800

    init

---
+++ XcodeCodeSnippet/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  XcodeCodeSnippet
+//
+//  Created by yongjie_zou on 2018/9/20.
+//  Copyright © 2018年 yongjie_zou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
